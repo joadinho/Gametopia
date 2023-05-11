f1d3ac44e74de223feb1c4c4548aab796e8ca79d
genaro <gen.espinoza@duocuc.cl>
2023-05-11 15:48:46 -0400

    Agregar nuevas url
    Agregar nuevas url y arreglo de la validacion imagenes, falto una para cada juego que era con la bse de datos

--- extension/urls.py
+++ extension/urls.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Pantalla,Registrarse
+from .views import Pantalla,Registrarse,Administrador,Agregar,Contacto,Login,Modificar,Olvidado,VerPerfil,WebServices,Xbox,Play,Pc,Nintendo
 
 urlpatterns = [
     path('',Pantalla,name="Pantalla"),
     path('Registrarse',Registrarse,name="Registrarse"),
+    path('Administrador',Administrador,name="Administrador"),
+    path('Agregar',Agregar,name="Agregar"),
+    path('Contacto',Contacto,name="Contacto"),
+    path('Login',Login,name="Login"),
+    path('Modificar',Modificar,name="Modificar"),
+    path('Olvidado',Olvidado,name="Olvidado"),
+    path('VerPerfil',VerPerfil,name="VerPerfil"),
+    path('WebServices',WebServices,name="WebServices"),
+    path('Xbox',Xbox,name="Xbox"),
+    path('Play',Play,name="Play"),
+    path('Pc',Modificar,name="Modificar"),
+    path('Nintendo',Nintendo,name="Nintendo"),
+
 ]
--- extension/views.py
+++ extension/views.py
@@ -5,4 +5,30 @@ def Pantalla(request):
     return render(request,'extension/Pantalla.html')
 
 def Registrarse(request):
-    return render(request,'extension/Registrarse.html')    
+    return render(request,'extension/Registrarse.html')
+def Administrador(request):
+    return render(request,'extension/administrador.html')
+def Agregar(request):
+    return render(request,'extension/AgregarJuego.html')
+def Contacto(request):
+    return render(request,'extension/Contacto.html')
+def Login(request):
+    return render(request,'extension/Login.html')
+def Modificar(request):
+    return render(request,'extension/Modificar.html')
+def Olvidado(request):
+    return render(request,'extension/olvidado.html')
+def VerPerfil(request):
+    return render(request,'extension/ver perfil.html')
+def WebServices(request):
+    return render(request,'extension/webServices.html')
+def Xbox(request):
+    return render(request,'extension/xbox.html')
+def Play(request):
+    return render(request,'extension/playstation.html')
+def Pc(request):
+    return render(request,'extension/pc.html')
+def Nintendo(request):
+    return render(request,'extension/nintendo.html')
+
+    
