41623ef5655ace40f0f80f1189a9de4ccc77c52d
JonyRiverosF <jo.riverosf@duocuc.cl>
2023-05-28 22:38:44 -0400

    listo admin

--- extension/admin.py
+++ extension/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import usuario,valoracion,comentario,rol,pregunta,plat_vid,plataforma,videojuego,vid_tien,tienda
+from .models import usuario,valoracion,comentario,rol,pregunta,plat_video,plataforma,videojuego,vid_tienda,tienda
 # Register your models here.
 admin.site.register(usuario)
 admin.site.register(valoracion)
 admin.site.register(comentario)
 admin.site.register(rol)
 admin.site.register(pregunta)
-admin.site.register(plat_vid)
+admin.site.register(plat_video)
 admin.site.register(plataforma)
 admin.site.register(videojuego)
-admin.site.register(vid_tien)
+admin.site.register(vid_tienda)
 admin.site.register(tienda)
