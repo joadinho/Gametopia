a42fb326ee6ca36b292990e739ca27a737f266d0
joadinho <joa.lisboa@duocuc.cl>
2023-06-08 00:37:35 -0400

    avance

--- extension/static/extension/Validaciones/olvidado.js
+++ extension/static/extension/Validaciones/olvidado.js
@@ -2,30 +2,12 @@ $(document).ready(function(){
     $("#formOlvidado").submit(function(e){
         
 
-        var email_ol = $("#emailO").val();
         var ComenJ = $("#ComeJ").val();
 
         let msjMostrarOlvidado = "";
         let enviar = false;
 
-        //valida email
-        if((email_ol).trim().indexOf('@', 0) == -1 || (email_ol).trim().indexOf('.', 0) == -1) {
-            msjMostrarOlvidado += "-El correo electrónico introducido es inválido. Debe contener un @";
-            enviar = true;
-            e.preventDefault();
-        }  
-        if(email_ol.trim() == ""){
-            msjMostrarOlvidado += "<br>-El campo correo no puede estar vacío";
-            enviar = true;
-            e.preventDefault();
-        }
-        var emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/; 
-
-        if (!emailRegex.test(email_ol)) {
-            msjMostrarOlvidado += "<br>-No debe comenzar con @";
-            enviar = true;
-            e.preventDefault();
-        }
+        
 
 
         if(enviar){
